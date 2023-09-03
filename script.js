3b59ce239ed4bf4adcd81b334537bff5337d0eb1
paulmatheson <iampaulmatheson@gmail.com>
2023-09-03 14:04:35 -0700

    Added script to change the theme as well as code to store the users selection locally.

--- script.js
+++ script.js
@@ -85,3 +85,43 @@ if (projectList !== null) {
         projectList.appendChild(projectDiv);
     });
 }
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleBtn = document.getElementById('theme-switch-btn')
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleBtn.classList.remove('fa-sun')
+        toggleBtn.classList.add('fa-moon')
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleBtn.classList.remove('fa-moon')
+        toggleBtn.classList.add('fa-sun')
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+console.log(currentTheme)
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
