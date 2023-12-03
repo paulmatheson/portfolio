935220dd8d7a4d7d1050a5375d3119e8aeb2198c
paulmatheson <iampaulmatheson@gmail.com>
2023-12-02 22:13:34 -0800

    Bigger commit today. Added one major feature in the form of a photo gallery using the Isotope.js and Lightgalle ryjs libaries. Made several minor modifications through out to accomodate.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ import projects from './projectData.js'
 import { renderProjects } from './projectRenderer.js'
 import { switchTheme } from './themeSwitch.js'
 import { menuAnimation } from "./menuAnimation.js"
+import photos from './photoData.js'
 
 const hmProjList = document.getElementById('hm-project-cards')
 const projectList = document.getElementById('project-list')
-const brand = document.getElementById('brand')
 
 const menuButton = document.getElementById('menu-button')
 const toggleLinks = document.querySelectorAll('.theme-toggle-link')
@@ -31,3 +31,41 @@ if (savedTheme) {
 }
 
 renderProjects(projects, hmProjList, projectList)
+
+
+
+let grid = document.querySelector('.grid');
+
+const htmlArray = [];
+
+photos.forEach(image => {
+    htmlArray.push(`
+        <a class="grid-item" href="${image.url}">
+            <img alt="img2" src="${image.url}" />
+        </a>
+    `);
+});
+
+// Join the HTML strings and set the innerHTML
+grid.innerHTML += htmlArray.join('');
+
+imagesLoaded(grid, function (instance) {
+    var iso = new Isotope('.grid', {
+        itemSelector: '.grid-item',
+        layoutMode: 'masonry',
+        masonry: {
+            gutter: 10
+        }
+    });
+})
+
+let gallery = document.getElementById('lightgallery')
+lightGallery(gallery, {
+    controls: true,
+    counter: false,
+    download: false,
+    thumbnail: true,
+    plugins: [
+        lgThumbnail
+    ]
+})
---
+++ scripts/photoData.js
@@ -0,0 +1,54 @@
+const photos = [
+    {
+        "url": "../photography/_6A45BBCE-71A2-4EDD-AAD6-6E837C39939E.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_7C2EA021-B2DF-4505-AFD0-6D3FD48A3405.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_8FC3F29C-C82D-4A97-BC21-73C93B3071C3.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_8FEB0CE7-D92F-4AB1-BF34-5D44DDE42BD2.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_10FF4F36-DF8C-4A83-A837-75B4BA156C20.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_53D3A10F-AA33-4A4D-8767-7757E8742B6C.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_86C499C2-254D-4177-B80D-3C60C0DDE7B1.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_1164DF8A-2A99-4028-80C0-215E1AEB7AB8.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_3558D12D-51FD-461A-8116-13E20C1FCAD2.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/_3677D789-BE40-4AE2-8B05-C55BD974DE5B.png",
+        "alt": "",
+        "category": ""
+    },
+]
+
+export default photos
