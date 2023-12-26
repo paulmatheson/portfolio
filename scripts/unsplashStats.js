b3879fa7a79903eb4c9584e0aaa2c1f9f42da061
paulmatheson <iampaulmatheson@gmail.com>
2023-12-26 11:21:02 -0800

    Updating stat code.

--- scripts/unsplashStats.js
+++ scripts/unsplashStats.js
@@ -1,11 +1,10 @@
-const api = `CX575TJrPQsXwNEifX5TqD6A-aLd8zU6Pcd4BaBGErk`
+const api = process.env.API_KEY;
 const url = `https://api.unsplash.com/users/paulmatheson/statistics/?client_id=${api}`
 const unsplashStats = document.getElementById('unsplash-stats')
 
 async function getUnsplashData() {
     const response = await fetch(url);
     const profileStats = await response.json();
-    console.log(profileStats);
 
     const views = new Intl.NumberFormat().format(profileStats.views.total)
     const downloads = new Intl.NumberFormat().format(profileStats.downloads.total)
