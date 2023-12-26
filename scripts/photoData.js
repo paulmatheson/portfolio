2b5527b9d393d75cea32869f825e295b5314029f
paulmatheson <iampaulmatheson@gmail.com>
2023-12-26 11:13:30 -0800

    Added new photos and an icon to the blog in the navigation.

--- scripts/photoData.js
+++ scripts/photoData.js
@@ -49,6 +49,26 @@ const photos = [
         "alt": "",
         "category": ""
     },
+    {
+        "url": "../photography/E5DAFC50-28B0-4CE2-A53D-62BDEDEAFF03.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/E967E505-DD1C-4CA9-B0EB-7B3C5C48E12F.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/ECC64E17-D4E2-4A29-A61E-612AF19B7B20.png",
+        "alt": "",
+        "category": ""
+    },
+    {
+        "url": "../photography/EF57AC4C-D538-4843-864A-E6B98E5FD9F9.png",
+        "alt": "",
+        "category": ""
+    }
 ]
 
 export default photos
