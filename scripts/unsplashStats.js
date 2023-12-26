7ccc1339d1bbfbfed41656a3e1d6542fa678fb78
paulmatheson <iampaulmatheson@gmail.com>
2023-12-26 12:52:56 -0800

    Fixing bugs.

--- scripts/unsplashStats.js
+++ scripts/unsplashStats.js
@@ -1,14 +1,19 @@
 const unsplashStats = document.getElementById('unsplash-stats')
 
 async function getUnsplashData() {
-    const response = await fetch('/.netlify/functions/unsplashStats');
-    const profileStats = await response.json();
+    try {
+        const response = await fetch('/.netlify/functions/unsplashStats');
+        const profileStats = await response.json();
+        console.log(response)
 
-    const views = new Intl.NumberFormat().format(profileStats.views.total)
-    const downloads = new Intl.NumberFormat().format(profileStats.downloads.total)
+        const views = new Intl.NumberFormat().format(profileStats.views.total)
+        const downloads = new Intl.NumberFormat().format(profileStats.downloads.total)
 
-    unsplashStats.innerHTML = `
-        <span id="photo-stat">${views}</span> views and <span id="photo-stat">${downloads}</span> downloads on <a href="https://unsplash.com/@paulmatheson" target="_blank">Unsplash ⧉.`
+        unsplashStats.innerHTML = `<span id="photo-stat">${views}</span> views and <span id="photo-stat">${downloads}</span> downloads on <a href="https://unsplash.com/@paulmatheson" target="_blank">Unsplash ⧉.`
+    } catch (error) {
+        console.error("Error fetching Unsplash data:", error);
+        unsplashStats.innerHTML = "Error fetching Unsplash data";
+    }
 }
 
 getUnsplashData()
