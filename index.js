3ad1743bb3cdc9ac77a8db2a9e868287a38aabdd
paulmatheson <iampaulmatheson@gmail.com>
2024-02-04 10:31:41 -0800

    Working Unsplash Stats

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const fetch = require('node-fetch');
+const api_key = process.env.API_KEY;
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Starting server at ${port}`);
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.get('/getUnsplashData', async (request, response) => {
+    console.log(request.url);
+
+    const api_url = `https://api.unsplash.com/users/paulmatheson//statistics/?client_id=${api_key}`
+
+    const fetch_response = await fetch(api_url)
+    const json = await fetch_response.json()
+    response.json(json)
+})
--- public/scripts/unsplashStats.js
+++ public/scripts/unsplashStats.js
@@ -1,18 +1,22 @@
-const fetchData = async () => {
+async function updateUnsplashStats() {
     try {
-        const response = await fetch('../functions/api-proxy', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ /* Your request payload */ }),
-        });
+        console.log('Fetching data...');
+        const response = await fetch('/getUnsplashData');
+        console.log('Response:', response);
 
-        const data = await response.json();
-        console.log('Data from API:', data);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+
+        const { views, downloads } = await response.json();
+        console.log('Data:', { views, downloads });
+
+        const unsplashStats = document.getElementById('unsplash-stats');
+        unsplashStats.innerHTML = `
+          <span id="photo-stat">${views.total.toLocaleString()}</span> views and <span id="photo-stat">${downloads.total.toLocaleString()}</span> downloads on <a href="https://unsplash.com/@paulmatheson" target="_blank">Unsplash ⧉.`;
     } catch (error) {
-        console.error('Error fetching data:', error.message);
+        console.error('Error updating Unsplash stats:', error.message);
     }
-};
+}
 
-fetchData();
+updateUnsplashStats();
