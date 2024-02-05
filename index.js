574b049ea03172b7f8230435eb639f94c5b1cb8f
paulmatheson <iampaulmatheson@gmail.com>
2024-02-04 16:36:54 -0800

    Fixing bugs.

--- index.js
+++ index.js
@@ -14,19 +14,16 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
 app.get('/getUnsplashData', async (request, response) => {
+    console.log(request.url);
+
+    const api_url = `https://api.unsplash.com/users/paulmatheson/statistics/?client_id=${api_key}`;
+
     try {
-        const api_url = `https://api.unsplash.com/users/paulmatheson/statistics/?client_id=${api_key}`;
-
-        console.log('Fetching data from Unsplash API:', api_url);
-
         const fetch_response = await fetch(api_url);
         const json = await fetch_response.json();
-
-        console.log('Unsplash API Response:', json);
-
         response.json(json);
     } catch (error) {
-        console.error('Error fetching Unsplash data:', error.message);
+        console.error('Error fetching Unsplash data:', error);
         response.status(500).json({ error: 'Internal Server Error' });
     }
 });
