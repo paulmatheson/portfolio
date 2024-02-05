bd2112928f9f429aaeadb0b8fc8509531ebcd860
paulmatheson <iampaulmatheson@gmail.com>
2024-02-04 20:01:04 -0800

    Removed not needed dependancies and modularized code better.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,6 @@ import projectsArr from './projectData.js'
 import { renderProjects } from './projectRenderer.js'
 import { switchTheme } from './themeSwitch.js'
 import { menuAnimation } from "./menuAnimation.js"
-import photos from './photoData.js'
 
 const hmProjList = document.getElementById('hm-project-cards')
 const projectList = document.getElementById('project-list')
@@ -31,39 +30,3 @@ if (savedTheme) {
 }
 
 renderProjects(projectsArr, hmProjList, projectList)
-
-let grid = document.querySelector('.grid');
-
-const htmlArray = [];
-
-photos.forEach(image => {
-    htmlArray.push(`
-        <a class="grid-item" href="${image.url}">
-            <img alt="img2" src="${image.url}" />
-        </a>
-    `);
-});
-
-// Join the HTML strings and set the innerHTML
-grid.innerHTML += htmlArray.join('');
-
-imagesLoaded(grid, function (instance) {
-    var iso = new Isotope('.grid', {
-        itemSelector: '.grid-item',
-        layoutMode: 'masonry',
-        masonry: {
-            gutter: 10
-        }
-    });
-})
-
-let gallery = document.getElementById('lightgallery')
-lightGallery(gallery, {
-    controls: true,
-    counter: false,
-    download: false,
-    thumbnail: true,
-    plugins: [
-        lgThumbnail
-    ]
-})
---
+++ public/scripts/photoScripts.js
@@ -0,0 +1,57 @@
+import photos from './photoData.js'
+
+async function updateUnsplashStats() {
+    try {
+        const response = await fetch('/getUnsplashData');
+        console.log('Response Data:', response);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+
+        const { views, downloads } = await response.json();
+
+        const unsplashStats = document.getElementById('unsplash-stats');
+        unsplashStats.innerHTML = `
+          <span id="photo-stat">${views.total.toLocaleString()}</span> views and <span id="photo-stat">${downloads.total.toLocaleString()}</span> downloads on <a href="https://unsplash.com/@paulmatheson" target="_blank">Unsplash ⧉.`;
+    } catch (error) {
+        console.error('Error updating Unsplash stats:', error.message);
+    }
+}
+
+let grid = document.querySelector('.grid');
+
+const htmlArray = [];
+
+photos.forEach(image => {
+    htmlArray.push(`
+        <a class="grid-item" href="${image.url}">
+            <img alt="img2" src="${image.url}" />
+        </a>
+    `);
+});
+
+grid.innerHTML += htmlArray.join('')
+
+imagesLoaded(grid, function (instance) {
+    var iso = new Isotope('.grid', {
+        itemSelector: '.grid-item',
+        layoutMode: 'masonry',
+        masonry: {
+            gutter: 10
+        }
+    });
+})
+
+let gallery = document.getElementById('lightgallery')
+lightGallery(gallery, {
+    controls: true,
+    counter: false,
+    download: false,
+    thumbnail: true,
+    plugins: [
+        lgThumbnail
+    ]
+})
+
+updateUnsplashStats();
