b2268ca1f08883b6ffbd2a014c203341bd719253
paulmatheson <iampaulmatheson@gmail.com>
2024-02-21 09:57:06 -0800

    Updated character limit for Twitter and code that tests and modifies the formatting of a quote that is too long for the Twitter character limit.

--- public/projects/the-quote-machine/js/script.js
+++ public/projects/the-quote-machine/js/script.js
@@ -1,27 +1,7 @@
-var colors = [
-	'#00A0B0',
-	'#6A4A3C',
-	'#CC333F',
-	'#EB6841',
-	'#EDC951',
-	'#3FB8AF',
-	'#7FC7AF',
-	'#FF9E9D',
-	'#C6A49A',
-	'#99B898',
-	'#2A363B',
-	'#F67280',
-	'#C06C84',
-	'#6C5B7B',
-	'#355C7D',
-	'#B3CC57',
-	'#FFBE40',
-	'#CC0C39',
-	'#E6781E',
-	'#02AAB0'
-];
+var colors = ['#00A0B0', '#6A4A3C', '#CC333F', '#EB6841', '#EDC951', '#3FB8AF', '#7FC7AF', '#FF9E9D', '#C6A49A', '#99B898', '#2A363B', '#F67280', '#C06C84', '#6C5B7B', '#355C7D', '#B3CC57', '#FFBE40', '#CC0C39', '#E6781E', '#02AAB0'];
 
 const apiUrl = 'https://api.quotable.io/random';
+const charLimit = 280 - 25 // Accounting for added formatting characters and default hashtag from url
 
 function getData() {
 	fetch(apiUrl)
@@ -31,6 +11,7 @@ function getData() {
 		})
 		.then(data => {
 			newQuote(data.author, data.content)
+			truncateString(data.content, data.author)
 		})
 		.catch(error => {
 			console.error('Fetch error:', error);
@@ -38,8 +19,8 @@ function getData() {
 }
 
 function newQuote(author, quote) {
-	var randomNumber = Math.floor(Math.random() * (colors.length));
-	var newColor = colors[randomNumber];
+	let randomNumber = Math.floor(Math.random() * (colors.length));
+	let newColor = colors[randomNumber];
 
 	document.body.style.backgroundColor = newColor;
 	quote = `"${quote}"`
@@ -58,31 +39,20 @@ function newQuote(author, quote) {
 };
 
 function tweet(message, author) {
-	window.open('https://twitter.com/intent/tweet?hashtags=thequotemachine&text=' + encodeURIComponent('"' + message + '" ' + author + " via"));
+	window.open('https://twitter.com/intent/tweet?hashtags=thequotemachine&text=' + encodeURIComponent(`"${message}" -${author} via`));
 }
 
-function truncateString(str, auth) {
-	var shorterStr = [];
-	var charCount = 0;
-	strSplit = str.split(" ");
-
-	if (str.length > (113 - auth.length)) {
-		for (var i = 0; i < strSplit.length; i++) {
-			if (charCount < (113 - auth.length)) {
-				charCount += strSplit[i].length + 1;
-				shorterStr.push(strSplit[i]);
-			}
-		}
-		shorterStr.pop();
-		return (shorterStr.join(" ") + "...");
-	} else {
-		return str;
+function truncateString(quote, author) {
+	// Check if quote length is longer than Twitter charLimit
+	if (quote.length + author.length > charLimit) {
+		return quote.substr(0, (charLimit - author.length))
 	}
+	return quote
 }
 
 $('.tweet').click(function () {
-	var currQuote = $('#quote').text();
-	var currAuthor = $('#author').text();
-	var truncatedString = truncateString(currQuote, currAuthor);
+	let currQuote = $('#quote').text().substr(1, $('#quote').text().length - 2) // To remove quotes
+	let currAuthor = $('#author').text();
+	let truncatedString = truncateString(currQuote, currAuthor);
 	tweet(truncatedString, currAuthor);
 });
