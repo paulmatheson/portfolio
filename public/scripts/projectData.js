2000852ba4a0402d667e5137066ea571a540818c
paulmatheson <iampaulmatheson@gmail.com>
2024-06-27 09:09:32 -0700

    Added logic to add icons to each project Type. Updated logic to adjust links to either open in new or same window for projects.

--- public/scripts/projectData.js
+++ public/scripts/projectData.js
@@ -5,55 +5,62 @@ const projectsArr = [
         link: "projects-frontend.html",
         type: "Collection",
         tags: ["HTML/CSS", "JS", "React", "Netlify", "App design"],
-        img: ""
+        img: "",
+        target: "_self"
     },
     {
         title: "Weather app",
         description: "A fully functional weather application that interacts with Visual Crossing's Easy Global Weather API.",
         link: "http://weatherapp.paulmatheson.net/",
-        type: "Web application",
+        type: "Web Application",
         tags: ["HTML/CSS", "JS", "Node.js", "API", "Netlify", "App design", "Chart.js"],
-        img: ""
+        img: "",
+        target: "_blank"
     },
     {
         title: "Drum machine",
         description: "A full-fledged interactive drum machine.",
         link: "https://codepen.io/illpill/full/xxYwLwq",
-        type: "Web application",
+        type: "Web Application",
         tags: ["HTML/CSS", "JS"],
-        img: ""
+        img: "",
+        target: "_blank"
     },
     {
         title: "Markdown previewer",
         description: "A markdown compiler built as part of my coursework. React, HTML/CSS(grid) and Javascript.",
         link: "https://codepen.io/illpill/full/OJREKXM",
-        type: "Web application",
+        type: "Web Application",
         tags: ["HTML/CSS", "JS", "React"],
-        img: ""
+        img: "",
+        target: "_blank"
     },
     {
         title: "Pomodoro Timer",
         description: "An easy to use and interactive Pomodoro Technique Timer.",
         link: "https://codepen.io/illpill/full/PQBbao/",
-        type: "Web application",
+        type: "Web Application",
         tags: ["HTML/CSS", "JS", "App design"],
-        img: ""
+        img: "",
+        target: "_blank"
     },
     {
         title: "Wikipedia Viewer",
         description: "An app built to search wikipedia. Built as part of my freeCodeCamp coursework.",
         link: "projects/wa/wiki/wikipedia-viewer.html",
-        type: "Web application",
+        type: "Web Application",
         tags: ["HTML/CSS", "JS", "API"],
-        img: ""
+        img: "",
+        target: "_self"
     },
     {
         title: "Quote Machine",
         description: "A random quote generator.",
         link: "projects/the-quote-machine/quote-machine.html",
-        type: "Web application",
+        type: "Web Application",
         tags: ["HTML/CSS", "JS"],
-        img: ""
+        img: "",
+        target: "_self"
     },
     {
         title: "Pong",
@@ -61,7 +68,8 @@ const projectsArr = [
         link: "projects/pong/pong.html",
         type: "Game",
         tags: ["HTML/CSS", "JS"],
-        img: ""
+        img: "",
+        target: "_self"
     },
     {
         title: "Simon Says",
@@ -69,7 +77,8 @@ const projectsArr = [
         link: "projects/simon/simon.html",
         type: "Game",
         tags: ["HTML/CSS", "JS"],
-        img: ""
+        img: "",
+        target: "_self"
     }
 ];
 
--- public/scripts/projectRenderer.js
+++ public/scripts/projectRenderer.js
@@ -1,8 +1,9 @@
 export function renderProjects(projectsArr, hmProjList, projectList) {
-
     // Loads cards to the home page
     if (hmProjList !== null) {
         for (let i = 0; i < 3; i++) {
+
+            const projectType = renderProjType(projectsArr[i])
 
             let article = document.createElement('article')
             let project = projectsArr[i]
@@ -13,12 +14,12 @@ export function renderProjects(projectsArr, hmProjList, projectList) {
             })
 
             article.innerHTML = `
-                <h5>${project.type}</h5>
+                <h5>${projectType}</h5>
                 <h3>${project.title}</h3>
                 
                 <p>${project.description}</p>
                 <img src="${project.img}" />
-                <a class="btn" href="${project.link}" target="_blank">Open Project ⧉</a>
+                <a class="btn" href="${project.link}" target="${project.target}">Open Project ⧉</a>
                 <ul class="tag-list">${tags.join("")}</ul>
             `
             hmProjList.appendChild(article)
@@ -33,13 +34,15 @@ export function renderProjects(projectsArr, hmProjList, projectList) {
         projectsArr.forEach(project => {
             let tags = []
 
+            const projectType = renderProjType(project)
+
             project.tags.forEach(tag => {
                 tags.push(`<li>${tag}</li>`)
             })
 
             const projectDiv = document.createElement('article');
             projectDiv.innerHTML = `
-            <h5>${project.type}</h5>
+            <h5>${projectType}</h5>
             <h3>${project.title}</h3>
             
             <p>${project.description}</p>
@@ -50,4 +53,22 @@ export function renderProjects(projectsArr, hmProjList, projectList) {
             projectList.appendChild(projectDiv);
         });
     }
+}
+
+function renderProjType(currProj) {
+    let projectType = ''
+
+    switch (currProj.type) {
+        case 'Collection':
+            projectType = `<ion-icon name="library-outline"></ion-icon> Collection`
+            break;
+        case 'Web Application':
+            projectType = `<ion-icon name="laptop-outline"></ion-icon> Web Application`
+            break
+        case 'Game':
+            projectType = `<ion-icon name="game-controller-outline"></ion-icon> Game`
+            break
+    }
+
+    return projectType
 }
