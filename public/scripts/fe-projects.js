64d6badaa3777c4b54f1d150c412bf8797ae2dd1
paulmatheson <iampaulmatheson@gmail.com>
2025-06-11 09:48:24 -0700

    Refactored code

--- public/scripts/fe-projects.js
+++ public/scripts/fe-projects.js
@@ -1,8 +1,11 @@
 import projList from './feProjectData.js'
 
-let projListDisplay = document.getElementById('fe-project-list')
+const feProjectList = document.getElementById('fe-project-list')
+const listIcon = document.getElementById('list-view')
+const gridIcon = document.getElementById('grid-view')
+const feHeader = document.getElementById('fe-header')
 
-for (let i = 0; i < projList.length && i <= 10; i++) {
+for (let i = 0; i < projList.length && i < 10; i++) {
     const li = document.createElement('li')
     li.className = 'proj-li'
     li.id = `${projList[i].id}`
@@ -11,8 +14,29 @@ for (let i = 0; i < projList.length && i <= 10; i++) {
         <a class="fe-link" href="${projList[i].gh}" target="_blank">GitHub <ion-icon name="open-outline"></ion-icon></a>
         <a class="fe-link" href="${projList[i].liveurl}" target="_blank">Live Link <ion-icon name="open-outline"></ion-icon></a>
     `
-    projListDisplay.appendChild(li)
+    feProjectList.appendChild(li)
 }
+
+listIcon.addEventListener('click', () => {
+    const allLi = feProjectList.querySelectorAll('li')
+    allLi.forEach(li => li.classList.remove('proj-li-card'))
+    feProjectList.classList.add('fe-project-list')
+    feProjectList.classList.remove('project-cards')
+    listIcon.classList.add('icon-selected')
+    gridIcon.classList.remove('icon-selected')
+    feHeader.style.display = 'grid'
+
+})
+
+gridIcon.addEventListener('click', () => {
+    feProjectList.classList.remove('fe-project-list')
+    feProjectList.classList.add('project-cards')
+    gridIcon.classList.add('icon-selected')
+    listIcon.classList.remove('icon-selected')
+    feHeader.style.display = 'none'
+    const allLi = feProjectList.querySelectorAll('li')
+    allLi.forEach(li => li.classList.add('proj-li-card'))
+})
 
 /*
 function feDisplaySwitch() {
