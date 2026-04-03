ff35d5b1f1ba48a8cdc3e147ce7e1d99603fc0a1
paulmatheson <iampaulmatheson@gmail.com>
2026-04-03 11:21:42 -0700

    Added a new app

--- public/scripts/projectData.js
+++ public/scripts/projectData.js
@@ -1,94 +1,117 @@
 const projectsArr = [
-    {
-        title: "Frontend Mentor Projects",
-        description: "A collection of solutions to Frontend Mentor challenges, showcasing clean and responsive UI design with all projects accessible on GitHub and live on Netlify.",
-        link: "projects-frontend.html",
-        type: "Collection",
-        tags: ["HTML/CSS", "JS", "React", "Netlify", "App design"],
-        img: "",
-        target: "_self"
-    },
-    {
-        title: "Days Left 365",
-        description: "A fun and interactive app to let you know that time is slipping by.",
-        link: "https://daysleft365.netlify.app/",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS", "Netlify", "App design"],
-        img: "",
-        target: "_blank"
-    },
-    {
-        title: "Weather app",
-        description: "A fully functional weather application that interacts with Visual Crossing's Easy Global Weather API.",
-        link: "http://weatherapp.paulmatheson.net/",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS", "Node.js", "API", "Netlify", "App design", "Chart.js"],
-        img: "",
-        target: "_blank"
-    },
-    {
-        title: "Drum machine",
-        description: "A full-fledged interactive drum machine.",
-        link: "https://codepen.io/illpill/full/xxYwLwq",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS"],
-        img: "",
-        target: "_blank"
-    },
-    {
-        title: "Markdown previewer",
-        description: "A markdown compiler built as part of my coursework. React, HTML/CSS(grid) and Javascript.",
-        link: "https://codepen.io/illpill/full/OJREKXM",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS", "React"],
-        img: "",
-        target: "_blank"
-    },
-    {
-        title: "Pomodoro Timer",
-        description: "An easy to use and interactive Pomodoro Technique Timer.",
-        link: "https://codepen.io/illpill/full/PQBbao/",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS", "App design"],
-        img: "",
-        target: "_blank"
-    },
-    {
-        title: "Wikipedia Viewer",
-        description: "An app built to search wikipedia. Built as part of my freeCodeCamp coursework.",
-        link: "projects/wiki/wikipedia-viewer.html",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS", "API"],
-        img: "",
-        target: "_self"
-    },
-    {
-        title: "Quote Machine",
-        description: "A random quote generator.",
-        link: "projects/the-quote-machine/quote-machine.html",
-        type: "Web Application",
-        tags: ["HTML/CSS", "JS"],
-        img: "",
-        target: "_self"
-    },
-    {
-        title: "Pong",
-        description: "Play my version of one of the first video games ever created",
-        link: "projects/pong/pong.html",
-        type: "Game",
-        tags: ["HTML/CSS", "JS"],
-        img: "",
-        target: "_self"
-    },
-    {
-        title: "Simon Says",
-        description: "Play the classic Simon game. See if you can make it to 20!",
-        link: "projects/simon/simon.html",
-        type: "Game",
-        tags: ["HTML/CSS", "JS"],
-        img: "",
-        target: "_self"
-    }
+  {
+    title: "Frontend Mentor Projects",
+    description:
+      "A collection of solutions to Frontend Mentor challenges, showcasing clean and responsive UI design with all projects accessible on GitHub and live on Netlify.",
+    link: "projects-frontend.html",
+    type: "Collection",
+    tags: ["HTML/CSS", "JS", "React", "Netlify", "App design"],
+    img: "",
+    target: "_self",
+  },
+  {
+    title: "Movie Database FE",
+    description:
+      "An app that displays popular movies using the The Movie Database (TMDB) API and allows users to add movies to their favorites using local storage. The app showcases clean and responsive UI design and is live on Netlify.",
+    link: "https://reactmovieprojectfav.netlify.app/",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS", "React", "Netlify", "API"],
+    img: "",
+    target: "_blank",
+  },
+  {
+    title: "Days Left 365",
+    description:
+      "A fun and interactive app to let you know that time is slipping by.",
+    link: "https://daysleft365.netlify.app/",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS", "Netlify", "App design"],
+    img: "",
+    target: "_blank",
+  },
+  {
+    title: "Weather app",
+    description:
+      "A fully functional weather application that interacts with Visual Crossing's Easy Global Weather API.",
+    link: "http://weatherapp.paulmatheson.net/",
+    type: "Web Application",
+    tags: [
+      "HTML/CSS",
+      "JS",
+      "Node.js",
+      "API",
+      "Netlify",
+      "App design",
+      "Chart.js",
+    ],
+    img: "",
+    target: "_blank",
+  },
+  {
+    title: "Drum machine",
+    description: "A full-fledged interactive drum machine.",
+    link: "https://codepen.io/illpill/full/xxYwLwq",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS"],
+    img: "",
+    target: "_blank",
+  },
+  {
+    title: "Markdown previewer",
+    description:
+      "A markdown compiler built as part of my coursework. React, HTML/CSS(grid) and Javascript.",
+    link: "https://codepen.io/illpill/full/OJREKXM",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS", "React"],
+    img: "",
+    target: "_blank",
+  },
+  {
+    title: "Pomodoro Timer",
+    description: "An easy to use and interactive Pomodoro Technique Timer.",
+    link: "https://codepen.io/illpill/full/PQBbao/",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS", "App design"],
+    img: "",
+    target: "_blank",
+  },
+  {
+    title: "Wikipedia Viewer",
+    description:
+      "An app built to search wikipedia. Built as part of my freeCodeCamp coursework.",
+    link: "projects/wiki/wikipedia-viewer.html",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS", "API"],
+    img: "",
+    target: "_self",
+  },
+  {
+    title: "Quote Machine",
+    description: "A random quote generator.",
+    link: "projects/the-quote-machine/quote-machine.html",
+    type: "Web Application",
+    tags: ["HTML/CSS", "JS"],
+    img: "",
+    target: "_self",
+  },
+  {
+    title: "Pong",
+    description: "Play my version of one of the first video games ever created",
+    link: "projects/pong/pong.html",
+    type: "Game",
+    tags: ["HTML/CSS", "JS"],
+    img: "",
+    target: "_self",
+  },
+  {
+    title: "Simon Says",
+    description: "Play the classic Simon game. See if you can make it to 20!",
+    link: "projects/simon/simon.html",
+    type: "Game",
+    tags: ["HTML/CSS", "JS"],
+    img: "",
+    target: "_self",
+  },
 ];
 
-export default projectsArr
+export default projectsArr;
